7a0da22bbf518ea1119b9ca9c88af20f90fd2de9
caslin <berkayunlukarabuk@gmail.com>
2016-02-29 16:12:37 +0200

    Added files via upload

---
+++ hareketli cisimleri tespit etme/WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(30, 90);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(358, 232);
+            this.videoSourcePlayer1.TabIndex = 0;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(30, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(202, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(313, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 356);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ hareketli cisimleri tespit etme/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision.Motion;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private FilterInfoCollection cihaz; //Cihazları comboBox da görüntüleyen nesne.
+        private VideoCaptureDevice kamera; //Bilgisayarda bulunan kamera vb. görüntü algılama cihazlarının seçileceği nesne.
+        MotionDetector Hareket_Yakala;  // Görüntüde hareketi algılama fonksiyonu (Aforge)
+        float Algi_Duzeyi;  //Hareketen eden cisimlerin belirleneceği ve atanacağı değişken
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Hareket_Yakala= new MotionDetector(new TwoFramesDifferenceDetector(),new MotionBorderHighlighting());
+            Algi_Duzeyi = 0;
+            cihaz = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo x in cihaz)
+            {
+                comboBox1.Items.Add(x.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            kamera = new VideoCaptureDevice(cihaz[comboBox1.SelectedIndex].MonikerString);
+            videoSourcePlayer1.VideoSource = kamera;
+            videoSourcePlayer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            videoSourcePlayer1.SignalToStop();
+        }
+
+        private void videoSourcePlayer1_NewFrame(object sender, ref Bitmap image)
+        {
+            Algi_Duzeyi = Hareket_Yakala.ProcessFrame(image);
+        }
+    }
+}
